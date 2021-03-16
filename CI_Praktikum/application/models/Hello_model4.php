1acbeaa73d9f79d89f72cd306259f37611288748
Aulia-Dinin-Putri <65695280+Aulia-Dinin-Putri@users.noreply.github.com>
2021-03-16 04:42:35 +0000

    Update Hello_model4.php

--- CI_Praktikum/application/models/Hello_model4.php
+++ CI_Praktikum/application/models/Hello_model4.php
@@ -1,5 +1,5 @@
 <?php
-	class Hello_model2 extends CI_Model{
+	class Hello_model4 extends CI_Model{
 		public function tampil_txt(){
 			$data = array('var1'=>'ini data satu', 'var2'=>'ini data dua');
 			return $data;
