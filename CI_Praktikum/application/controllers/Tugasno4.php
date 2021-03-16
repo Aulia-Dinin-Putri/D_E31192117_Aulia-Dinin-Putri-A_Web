8fe7af52dafc8eeab45676b4d35a4bcc38f0af5c
Aulia-Dinin-Putri <65695280+Aulia-Dinin-Putri@users.noreply.github.com>
2021-03-16 04:33:39 +0000

    Update Tugasno4.php

--- CI_Praktikum/application/controllers/Tugasno4.php
+++ CI_Praktikum/application/controllers/Tugasno4.php
@@ -1,5 +1,5 @@
 <?php
-	class Soal4 extends CI_Controller{
+	class Tugasno4 extends CI_Controller{
 		public function index() {
 			$this->load->model('Tugasno4');
 			$data = $this->Tugasno4->tampil_txt();
