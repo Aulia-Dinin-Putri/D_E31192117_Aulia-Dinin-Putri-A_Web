79fe2e7dee48b5b3c2b72fa3bf181330b01e5465
Aulia-Dinin-Putri <65695280+Aulia-Dinin-Putri@users.noreply.github.com>
2021-03-17 22:11:14 +0700

    Praktikum ke-7 Web Framework

---
+++ CI_Minggu7/application/views/variabelakhir.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head><title>Controller dan View lebih dari 1 Variabel</title></head>
+<body>
+
+	<h2>Mengirim Data dari Controller ke View</h2>
+
+	<?php
+	foreach ($tampil as $row) {
+		echo "Nama: " . $row['Nama'];
+		echo "NIM: " . $row['NIM'];
+		echo "Golongan: " . $row['Gol'];
+	}
+</body>
+</html>
