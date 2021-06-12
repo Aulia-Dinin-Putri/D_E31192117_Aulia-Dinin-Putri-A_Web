8bf3d0bf0bfa935f223b8a1a0741ad090de94f75
Aulia-Dinin-Putri <65695280+Aulia-Dinin-Putri@users.noreply.github.com>
2021-06-12 18:37:52 +0000

    delete

--- CI_Praktikum/application/models/Hello_model3.php
+++
@@ -1,8 +0,0 @@
-<?php
-	class Hello_model3 extends CI_Model{
-		public function tampil_txt(){
-			$txt = 'Hello World dari CI Model';
-			return $txt;
-		}
-	}
-?>
--- CI_Praktikum/application/models/Hello_model4.php
+++
@@ -1,8 +0,0 @@
-<?php
-	class Hello_model4 extends CI_Model{
-		public function tampil_txt(){
-			$data = array('var1'=>'ini data satu', 'var2'=>'ini data dua');
-			return $data;
-		}
-	}
-?>
