664e1c925a0f76cc674899c0c8fcc9e476f3f8c0
Aulia-Dinin-Putri <65695280+Aulia-Dinin-Putri@users.noreply.github.com>
2021-06-12 18:57:22 +0000

    praktikum

---
+++ CI_Minggu11_CRUD2/application/views/Admin/_Template/content.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                	</div>
+                	<!-- Content Row -->
+          			<?php echo $isi; ?>
+          		</div>
+
+</html>
